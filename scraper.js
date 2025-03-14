43a38866c819a832be8762c88771f90360562daf
hany66 <hanyshaker@hotmail.com>
2025-03-15 00:09:47 +0200

    commit7

--- scraper.js
+++ scraper.js
@@ -1,11 +1,11 @@
 process.env.PUPPETEER_CACHE_DIR = 'E:\\tmdb-scraper\\.cache\\puppeteer';
 const puppeteer = require("puppeteer-core");
-const config = require('./puppeteer.cjs');
+const config = require('./puppeteer.config.cjs');
 const path = require('path');
 // Define the cache directory
 process.env.PUPPETEER_CACHE_DIR = 'E:\\tmdb-scraper\\.cache\\puppeteer';
 
-const cacheDirectory = path.join(__dirname, '.cache', 'puppeteer');
+//const cacheDirectory = path.join(__dirname, '.cache', 'puppeteer');
 //console.log('Cache= ',cacheDirectory);
 //console.log("Using Puppeteer cache directory:", process.env.PUPPETEER_CACHE_DIR);
 //console.log("Using Puppeteer download path:", process.env.PUPPETEER_DOWNLOAD_PATH);
@@ -15,11 +15,13 @@ async function scrapeMovies() {
         headless: "new",
         //executablePath: puppeteer.executablePath(), // Use Puppeteer's Chromium
        // executablePath: 'E:\\tmdb-scraper\\chrome-win\\chrome-win\\chrome.exe' ,// Replace with actual path
-        executablePath:"C:\\temp\\puppeteer-cache\\chrome\\win64-134.0.6998.35\\chrome-win64\\chrome.exe",
+         executablePath:"C:\\temp\\puppeteer-cache\\chrome\\win64-134.0.6998.35\\chrome-win64\\chrome.exe",
+      //  executablePath:"process.env.PUPPETEER_EXECUTABLE_PATH",
         args: ["--no-sandbox", "--disable-setuid-sandbox",
         ],
+        cacheDirectory: config.cacheDirectory // Apply custom cache director
         // Set the cache directory
-        cacheDirectory: cacheDirectory,
+        //cacheDirectory: cacheDirectory,
     });
     const page = await browser.newPage();
     await page.goto("https://www.themoviedb.org/movie", { waitUntil: "domcontentloaded" });
