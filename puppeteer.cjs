e9b35f012b23466c428f855872568acb42e38f81
hany66 <hanyshaker@hotmail.com>
2025-03-14 23:22:33 +0200

    commit3

--- puppeteer.cjs
+++ puppeteer.cjs
@@ -1,4 +1,15 @@
+/*
 const { join } = require('path');
+
+/!**
+ * @type {import("puppeteer").Configuration}
+ *!/
+module.exports = {
+    // Changes the cache location for Puppeteer.
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};*/
+const { win32 } = require('path');
+const { join } = win32; // Explicitly use Windows-style path joining
 
 /**
  * @type {import("puppeteer").Configuration}
